69143908ba1c6e8f982185ee5faee13f31cbcea9
lcolladotor <lcollado@jhsph.edu>
2018-12-07 17:23:19 -0500

    Specify --memory when calling plink

--- FUSION.compute_weights.R
+++ FUSION.compute_weights.R
@@ -79,7 +79,7 @@ weights.bslmm = function( input , bv_type , snp , out=NA ) {
 weights.lasso = function( input , hsq , snp , out=NA ) {
 	if ( is.na(out) ) out = paste(input,".LASSO",sep='')
 
-	arg = paste( opt$PATH_plink , " --allow-no-sex --bfile " , input , " --lasso " , hsq , " --out " , out , sep='' )
+	arg = paste( opt$PATH_plink , " --allow-no-sex --bfile " , input , " --lasso " , hsq , " --out " , out , " --memory 10000", sep='' )
 	system( arg , ignore.stdout=SYS_PRINT,ignore.stderr=SYS_PRINT )
 	if ( !file.exists(paste(out,".lasso",sep='')) ) {
 	cat( paste(out,".lasso",sep='') , " LASSO output did not exist\n" )
@@ -199,7 +199,7 @@ if ( !is.na(opt$covar) ) {
 
 geno.file = opt$tmp
 # recode to the intersection of samples and new phenotype
-arg = paste( opt$PATH_plink ," --allow-no-sex --bfile ",opt$bfile," --pheno ",pheno.file," --keep ",pheno.file," --make-bed --out ",geno.file,sep='')
+arg = paste( opt$PATH_plink ," --allow-no-sex --bfile ",opt$bfile," --pheno ",pheno.file," --keep ",pheno.file," --make-bed --out ",geno.file, " --memory 10000", sep='')
 system(arg , ignore.stdout=SYS_PRINT,ignore.stderr=SYS_PRINT)
 
 # --- HERITABILITY ANALYSIS
@@ -207,7 +207,7 @@ if ( is.na(opt$hsq_set) ) {
 if ( opt$verbose >= 1 ) cat("### Estimating heritability\n")
 
 # 1. generate GRM
-arg = paste( opt$PATH_plink," --allow-no-sex --bfile ",opt$tmp," --make-grm-bin --out ",opt$tmp,sep='')
+arg = paste( opt$PATH_plink," --allow-no-sex --bfile ",opt$tmp," --make-grm-bin --out ",opt$tmp, " --memory 10000", sep='')
 system(arg , ignore.stdout=SYS_PRINT,ignore.stderr=SYS_PRINT)
 
 # 2. estimate heritability
@@ -293,7 +293,7 @@ for ( i in 1:opt$crossval ) {
 	# hide current fold
 	cv.file = paste(opt$tmp,".cv",sep='')
 	write.table( cv.train , quote=F , row.names=F , col.names=F , file=paste(cv.file,".keep",sep=''))	
-	arg = paste( opt$PATH_plink ," --allow-no-sex --bfile ",opt$tmp," --keep ",cv.file,".keep --out ",cv.file," --make-bed",sep='')
+	arg = paste( opt$PATH_plink ," --allow-no-sex --bfile ",opt$tmp," --keep ",cv.file,".keep --out ",cv.file," --make-bed", " --memory 10000", sep='')
 	system(arg , ignore.stdout=SYS_PRINT,ignore.stderr=SYS_PRINT)
 
 	for ( mod in 1:M ) {
